6f05eb871b7bafbc6941712e58e6a6647b071876
Álvaro Fernando <alvarofernandoms@gmail.com>
2015-10-09 16:13:52 -0300

    Fixing the list of instructions in dragArea
    Signed-off-by: Álvaro Fernando <alvarofernandoms@gmail.com>

--- app/src/main/java/com/br/robot_app_2/MainActivity.java
+++ app/src/main/java/com/br/robot_app_2/MainActivity.java
@@ -54,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
 
         private int resource;
 
-        private int count = -1;
+        //private int count = 0;
 
         public MyTouchListener(int resource) {
             this.resource = resource;
@@ -63,47 +63,50 @@ public class MainActivity extends AppCompatActivity {
         public boolean onTouch(View view, MotionEvent motionEvent) {
             //count++;
             LinearLayout dragArea = (LinearLayout)findViewById(R.id.dragArea);
+            LinearLayout linha1 = (LinearLayout) dragArea.findViewById(R.id.linha1);
+            int count1 = linha1.getChildCount();
+            LinearLayout linha2 = (LinearLayout) dragArea.findViewById(R.id.linha2);
+            int count2 = linha2.getChildCount();
+            LinearLayout linha3 = (LinearLayout) dragArea.findViewById(R.id.linha3);
+            int count3 = linha3.getChildCount();
+            LinearLayout linha4 = (LinearLayout) dragArea.findViewById(R.id.linha4);
+            int count4 = linha4.getChildCount();
             if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                if (count == 0 || count < 4) {
+                if (count1 == 0 || count1 < 4) {
                     ImageView v1 = new ImageView(getApplication());
                     v1.setImageResource(resource);
-                    LinearLayout linha1 = (LinearLayout) dragArea.findViewById(R.id.linha1);
                     linha1.addView(v1);
-                    count++;
+                    //count++;
                     return true;
                 }
-                else if (count == 4 || count < 8){
+                else if (count2 == 0 || count2 < 4){
                     ImageView v1 = new ImageView(getApplication());
                     v1.setImageResource(resource);
-                    LinearLayout linha2 = (LinearLayout) dragArea.findViewById(R.id.linha2);
                     linha2.addView(v1);
-                    count++;
+                    //count++;
                     return true;
                 }
-                else if (count == 8 || count < 12){
+                else if (count3 == 0 || count3 < 4){
                     ImageView v1 = new ImageView(getApplication());
                     v1.setImageResource(resource);
-                    LinearLayout linha3 = (LinearLayout) dragArea.findViewById(R.id.linha3);
                     linha3.addView(v1);
-                    count++;
+                    //count++;
                     return true;
                 }
-                else if (count == 12 || count < 16){
+                else if (count4 == 0 || count4 < 4){
                     ImageView v1 = new ImageView(getApplication());
                     v1.setImageResource(resource);
-                    LinearLayout linha4 = (LinearLayout) dragArea.findViewById(R.id.linha4);
                     linha4.addView(v1);
-                    count++;
+                    //count++;
                     return true;
                 }
                 //count++;
             } else return false;
 
 
+            //count++;
             return false;
         }
-
-
 
     }
 }
